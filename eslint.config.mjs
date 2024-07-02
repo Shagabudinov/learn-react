6668eae97e18623cf3e2ae54963f148cea192465
Shagabudinov <cherrygumfd@mail.ru>
2024-07-02 20:16:52 +0300

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,60 @@
+import globals from "globals";
+import js from "@eslint/js";
+import tsEslint from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+import eslintReact from "eslint-plugin-react";
+import eslintReactHooks from "eslint-plugin-react-hooks";
+import eslintReactRefresh from "eslint-plugin-react-refresh";
+import prettierPlugin from "eslint-plugin-prettier";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default [
+  {
+    plugins: {
+      "@typescript-eslint": tsEslint,
+      "react-hooks": eslintReactHooks,
+      react: eslintReact,
+      "react-refresh": eslintReactRefresh,
+      prettier: prettierPlugin,
+    },
+    ignores: [
+      "node_modules",
+      "dist",
+      "eslint.config.mjs",
+      "webpack.config.js",
+      "dist/bundle.js",
+    ],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2023,
+      },
+      parser: tsParser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        ecmaVersion: 2023,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      ...tsEslint.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      "react/jsx-no-target-blank": "off",
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
+      semi: ["error", "always"],
+      "prefer-const": ["error"],
+    },
+    linterOptions: {
+      noInlineConfig: true,
+    },
+    files: ["**/*.{js,jsx,ts,tsx}"],
+  },
+];
---
+++ webpack.config.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.ts?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "dist", "index.html"),
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
+};
